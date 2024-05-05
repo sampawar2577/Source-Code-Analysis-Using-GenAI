95b6313acac52c29a64ad4fc091c736b70691768
sampawar2577 <sampawar2777@gmail.com>
2024-05-05 10:07:30 +0530

    req updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Generative AI Project",
+    version = "0.0.0",
+    author = "Sam Pawar",
+    author_email = "sampawar2777@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
